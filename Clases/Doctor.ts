e184a09d62ef29895e131ab3edb419bbe958e351
CRONOXT <59267988+CRONOXT@users.noreply.github.com>
2022-05-13 18:29:35 -0400

    Separacion de clases
    Se separaron las clases en distintos archivos para evitar conflictos a la hora de hacer push

---
+++ Clases/Doctor.ts
@@ -0,0 +1,54 @@
+import {Persona} from "./Persona"
+import {Especialidad} from "./Especialidad"
+import {HistorialMedico} from "./HistorialMedico"
+import {Cita} from "./Cita"
+
+
+export class Doctor extends Persona{
+  
+    constructor (id:string,Nombre:string,Apellido:string,private especialidad:Especialidad[]){
+        super(id,Nombre,Apellido)
+        this.especialidad= especialidad;
+    }
+
+    get _id():string{
+        return this.id;
+    }  
+    get _Nombre():string{
+        return this.Nombre;
+    }  
+    get _Apellido():string{
+        return this.Apellido;
+    }  
+    get _especialidad():Especialidad[]{
+        return this.especialidad;
+    }  
+    set _id(value:string){
+        this.id=value;
+    }
+    set _Nombre(value:string){
+        this.Nombre=value;
+    }
+    set _Apellido(value:string){
+        this.Apellido=value;
+    }
+    set _especialidad(value:Especialidad[]){
+        this.especialidad=value;
+    }
+    modificar(): void {
+        
+    }   ;
+    consultar(): void {
+        
+    }   ;
+    editarHistoriaMedica(historialMedico:HistorialMedico):HistorialMedico{
+
+    }   ;
+    crearHistoriaMedica(historial:Cita):HistorialMedico{
+
+    }   ;
+    agregarCita():void{
+
+    }   ;
+
+}
---
+++ Clases/Especialidad.ts
@@ -0,0 +1,26 @@
+import {ExamenMedicoTipo} from "./ExamenMedicoTipo"
+export class Especialidad{
+
+
+    constructor (private id: string,private nombre: string,private examenesRequeridos:ExamenMedicoTipo[]){
+
+    }
+    get _id():string{
+        return this.id;
+    }  
+    get _nombre():string{
+        return this.nombre;
+    }
+    get _examenesRequeridos():ExamenMedicoTipo[]{
+        return this.examenesRequeridos;
+    }
+    set _id(value:string){
+        this.id=value;
+    }
+    set _nombre(value:string){
+        this.nombre=value;
+    }
+    set _examenesRequeridos(value:ExamenMedicoTipo[]){
+        this.examenesRequeridos=value;
+    }
+}
